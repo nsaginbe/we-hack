71c075c1e0829acb1954e86deb1b72082443449e
Nurgisa Saginbek <nurgisasaginbek@gmail.com>
2025-05-03 21:12:09 +0500

    initial commit

---
+++ backend/src/main/java/org/project/qysqasha/QysqashaApplication.java
@@ -0,0 +1,13 @@
+package org.project.qysqasha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QysqashaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QysqashaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/project/qysqasha/QysqashaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.qysqasha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QysqashaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
